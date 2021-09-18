b41057988b645f0273870cf2575113941226a86b
YashasTadikamalla <80558237+YashasTadikamalla@users.noreply.github.com>
2021-09-18 20:55:34 +0530

    Add files via upload

--- Assignment5/codes/Assignment5.py
+++ Assignment5/codes/Assignment5.py
@@ -16,7 +16,7 @@ I=I.reshape(2,2)
 
 # calculating V,u,f
 V=(pow(n_norm,2)*I)-(pow(e,2)*(n@n.T))
-u=(c*pow(n_norm,2)*n)-(pow(n_norm,2)*F)
+u=(c*pow(e,2)*n)-(pow(n_norm,2)*F)
 f=(pow(n_norm,2)*pow(F_norm,2))-(pow(c,2)*pow(e,2))
 
 print(V)
@@ -26,7 +26,22 @@ print(f)
 fig=plt.figure()
 ax = fig.add_subplot(111)
 len = 10000
-y = np.linspace(-10,10,len)
+y = np.linspace(6,15,len)
+
+def hyperbola_gen(y):
+    x = np.sqrt(3*((y**2)-36))
+    return x
+
+#Generating the Standard Hyperbola
+x = hyperbola_gen(y)
+xStandardLeft = np.vstack((-x,y))
+xStandardRight = np.vstack((x,y))
+
+#Plotting the standard hyperbola
+plt.plot(xStandardLeft[0,:],xStandardLeft[1,:],color='b')
+plt.plot(xStandardRight[0,:],xStandardRight[1,:],color='b')
+
+y = np.linspace(-15,-6,len)
 
 def hyperbola_gen(y):
     x = np.sqrt(3*((y**2)-36))
@@ -42,31 +57,36 @@ A = np.array([0,12])
 B = np.array([0,-12])
 C = np.array([0,6])
 D = np.array([0,-6])
+E = np.array([18,12])
+F = np.array([18,-12])
+G = np.array([-18,12])
+H = np.array([-18,-12])
 
 #Plotting the standard hyperbola
 plt.plot(xStandardLeft[0,:],xStandardLeft[1,:],label='Hyperbola',color='b')
 plt.plot(xStandardRight[0,:],xStandardRight[1,:],color='b')
 
 # Plotting the foci and vertices
-plt.plot(A[0],A[1],'o',color='g',label='Focus1')
+plt.plot(A[0],A[1],'o',color='g',label='Focii')
 plt.text(0.5 ,11.5,'F1(0,12)')
-plt.plot(B[0],B[1],'o',color='g',label='Focus2')
+plt.plot(B[0],B[1],'o',color='g')
 plt.text(0.5 ,-12.5,'F2(0,-12)')
-plt.plot(C[0],C[1],'o',color='r',label='Vertex1')
+plt.plot(C[0],C[1],'o',color='r',label='Vertices')
 plt.text(0.5 ,5.5,'B1(0,6)')
-plt.plot(D[0],D[1],'o',color='r',label='Vertex2')
+plt.plot(D[0],D[1],'o',color='r')
 plt.text(0.5 ,-6.5,'B2(0,-6)')
+plt.plot(E[0],E[1],'o',color='orange')
+plt.plot(F[0],F[1],'o',color='orange')
+plt.plot(G[0],G[1],'o',color='orange')
+plt.plot(H[0],H[1],'o',color='orange')
+plt.plot([E[0],G[0]],[E[1],G[1]],'y',label='Latus Rectum')
+plt.plot([F[0],H[0]],[F[1],H[1]],'y')
+plt.plot([-20,20],[3,3],'g',label='Directrices')
+plt.plot([-20,20],[-3,-3],'g')
 
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-plt.legend(loc='best')
-plt.grid() # minor
+plt.legend(loc='right')
+plt.grid() 
 plt.axis('equal')
 plt.show()
-
-
-# In[ ]:
-
-
-
-
